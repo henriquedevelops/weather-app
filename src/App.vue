67f50c629603218d992795a4a61a7e9d37d564f7
Henrique Buzon <henriquethedeveloper@gmail.com>
2025-11-28 21:24:29 -0300

    Created vue project

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts"></script>
+
+<template>
+  <main class="main">
+    <h1>You did it!</h1>
+    <p>
+      Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
+      documentation
+    </p>
+  </main>
+</template>
+
+<style scoped lang="scss">
+.main {
+  background-color: red;
+  width: 100%;
+  height: 100%;
+}
+</style>
