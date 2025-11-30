5261ea9a9aa75b22e3cf119c76d9a349d8f47f37
Henrique Buzon <henriquethedeveloper@gmail.com>
2025-11-30 15:27:40 -0300

    Add weather API environment variables to ImportMeta interface

--- env.d.ts
+++ env.d.ts
@@ -1,2 +1,11 @@
 /// <reference types="vite/client" />
 /// <reference types="unplugin-icons/types/vue" />
+
+interface ImportMetaEnv {
+  readonly VITE_WEATHER_API_KEY: string
+  readonly VITE_WEATHER_API_BASE_URL: string
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv
+}
